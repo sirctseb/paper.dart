80acc22508ca3b872abc6700a6c1f367aaccac59
Christopher Best <bestchris@gmail.com>
2012-08-05 19:18:46 -0400

    started file for ported tests

---
+++ test/tests/Tests.dart
@@ -0,0 +1,8 @@
+#import("../../../../Downloads/dart\ 2/dart-sdk/lib/unittest/unittest.dart");
+#import("../../src/basic/Point.dart");
+#library("paper.dart.test");
+#source("./Point.dart");
+
+void main() {
+
+}
