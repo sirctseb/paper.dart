7ae0ddc509e874e83fd230dee8c7db3959289746
Christopher Best <bestchris@gmail.com>
2012-08-08 11:30:14 -0400

    put actual test calls in group in top level fn

--- test/tests/Size.dart
+++ test/tests/Size.dart
@@ -14,28 +14,31 @@
  * All rights reserved.
  */
 
-module('Size');
-test('new Size(10, 20)', function() {
-  var size = new Size(10, 20);
-  equals(size.toString(), '{ width: 10, height: 20 }');
-});
+SizeTests() {
+  group("Size Tests", () {
+    test('new Size(10, 20)', function() {
+      var size = new Size(10, 20);
+      equals(size.toString(), '{ width: 10, height: 20 }');
+    });
 
-test('new Size([10, 20])', function() {
-  var size = new Size([10, 20]);
-  equals(size.toString(), '{ width: 10, height: 20 }');
-});
+    test('new Size([10, 20])', function() {
+      var size = new Size([10, 20]);
+      equals(size.toString(), '{ width: 10, height: 20 }');
+    });
 
-test('new Size({width: 10, height: 20})', function() {
-  var size = new Size({width: 10, height: 20});
-  equals(size.toString(), '{ width: 10, height: 20 }');
-});
+    test('new Size({width: 10, height: 20})', function() {
+      var size = new Size({width: 10, height: 20});
+      equals(size.toString(), '{ width: 10, height: 20 }');
+    });
 
-test('new Size(new Point(10, 20))', function() {
-  var size = new Size(new Point(10, 20));
-  equals(size.toString(), '{ width: 10, height: 20 }');
-});
+    test('new Size(new Point(10, 20))', function() {
+      var size = new Size(new Point(10, 20));
+      equals(size.toString(), '{ width: 10, height: 20 }');
+    });
 
-test('new Size({ x: 10, y: 20})', function() {
-  var size = new Size({x: 10, y: 20});
-  equals(size.toString(), '{ width: 10, height: 20 }');
-});
+    test('new Size({ x: 10, y: 20})', function() {
+      var size = new Size({x: 10, y: 20});
+      equals(size.toString(), '{ width: 10, height: 20 }');
+    });
+  });
+}
