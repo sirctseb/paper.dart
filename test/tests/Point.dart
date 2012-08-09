ce3c065ac7cbe93b30df135ee761b8c23b144525
Christopher Best <bestchris@gmail.com>
2012-08-09 08:40:03 -0400

    Put real Size back in Point test

--- test/tests/Point.dart
+++ test/tests/Point.dart
@@ -32,8 +32,7 @@
       });
 
     test('new Point(new Size(10, 20))', () {
-      // TODO until Size ported
-      var point = new Point();//new Size(10, 20));
+      var point = new Size(10, 20));
       expect(point.toString(), equals('{ x: 10, y: 20 }'));
       });
 
