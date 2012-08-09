cc5f294bcd2325f26b74bd2d7cd1a7e71660746e
Christopher Best <bestchris@gmail.com>
2012-08-09 08:20:57 -0400

    import rectangle and size libraries
    also update unittest location for this comp

--- test/tests/Tests.dart
+++ test/tests/Tests.dart
@@ -1,6 +1,8 @@
 #library("paper.dart.test");
-#import("../../../../Downloads/dart 2/dart-sdk/lib/unittest/unittest.dart");
+#import("../../../../../Desktop/dart/dart-sdk/lib/unittest/unittest.dart");
 #import("../../src/basic/Point.dart");
+#import("../../src/basic/Rectangle.dart");
+#import("../../src/basic/Size.dart");
 #source("./Point.dart");
 #source("./Rectangle.dart");
 #source("./Size.dart");
