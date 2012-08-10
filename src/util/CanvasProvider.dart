bc9488725da82e7c0ac17388ce882ee8cc854759
Christopher Best <bestchris@gmail.com>
2012-08-10 15:32:28 -0400

    tabs to spaces

--- src/util/CanvasProvider.dart
+++ src/util/CanvasProvider.dart
@@ -20,35 +20,35 @@
 // TODO: Run through the canvas array to find a canvas with the requested
 // width / height, so we don't need to resize it?
 var CanvasProvider = {
-	canvases: [],
-	getCanvas: function(size) {
-		if (this.canvases.length) {
-			var canvas = this.canvases.pop();
-			// If they are not the same size, we don't need to clear them
-			// using clearRect and visa versa.
-			if ((canvas.width != size.width)
-					|| (canvas.height != size.height)) {
-				canvas.width = size.width;
-				canvas.height = size.height;
-			} else {
-				// +1 is needed on some browsers to really clear the borders
-				canvas.getContext('2d').clearRect(0, 0,
-						size.width + 1, size.height + 1);
-			}
-			return canvas;
-		} else {
+  canvases: [],
+  getCanvas: function(size) {
+    if (this.canvases.length) {
+      var canvas = this.canvases.pop();
+      // If they are not the same size, we don't need to clear them
+      // using clearRect and visa versa.
+      if ((canvas.width != size.width)
+          || (canvas.height != size.height)) {
+        canvas.width = size.width;
+        canvas.height = size.height;
+      } else {
+        // +1 is needed on some browsers to really clear the borders
+        canvas.getContext('2d').clearRect(0, 0,
+            size.width + 1, size.height + 1);
+      }
+      return canvas;
+    } else {
 /*#*/ if (options.browser) {
-			var canvas = document.createElement('canvas');
-			canvas.width = size.width;
-			canvas.height = size.height;
-			return canvas;
+      var canvas = document.createElement('canvas');
+      canvas.width = size.width;
+      canvas.height = size.height;
+      return canvas;
 /*#*/ } else { // !options.browser
-			return new Canvas(size.width, size.height);
+      return new Canvas(size.width, size.height);
 /*#*/ } // !options.browser
-		}
-	},
+    }
+  },
 
-	returnCanvas: function(canvas) {
-		this.canvases.push(canvas);
-	}
+  returnCanvas: function(canvas) {
+    this.canvases.push(canvas);
+  }
 };
