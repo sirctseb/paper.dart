d6d31434127016b9d3cf65d3da4e9e1f7f0d7343
Christopher Best <bestchris@gmail.com>
2012-08-10 13:40:11 -0400

    added color tests to main test fn

--- test/tests/Tests.dart
+++ test/tests/Tests.dart
@@ -1,14 +1,17 @@
 #library("paper.dart.test");
 #import("../../../../../Desktop/dart/dart-sdk/lib/unittest/unittest.dart");
 #import("../../src/basic/Basic.dart");
+#import("../../src/color/Color.dart");
 #source("./Point.dart");
 #source("./Rectangle.dart");
 #source("./Size.dart");
 #source("./Line.dart");
+#source("./Color.dart");
 
 void main() {
 	PointTests();
 	RectangleTests();
 	SizeTests();
 	LineTests();
+	ColorTests();
 }
