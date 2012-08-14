b4c76fedc0b4a554b376073d70f14121a9d3525a
Christopher Best <bestchris@gmail.com>
2012-08-14 15:16:06 -0400

    ported tests

--- test/tests/Item.dart
+++ test/tests/Item.dart
@@ -21,15 +21,9 @@ ItemTests() {
       var path = new Path();
       var secondDoc = new Project();
       var copy = path.copyTo(secondDoc);
-      equals(() {
-        return secondDoc.activeLayer.children.indexOf(copy) != -1;
-      }, true);
-      equals(() {
-        return project.activeLayer.children.indexOf(copy) == -1;
-      }, true);
-      equals(() {
-        return copy != path;
-      }, true);
+      expect(secondDoc.activeLayer.children.indexOf(copy) != -1, true);
+      expect(project.activeLayer.children.indexOf(copy) == -1, true);
+      expect(copy != path, true);
     });
 
     test('copyTo(layer)', () {
@@ -38,33 +32,23 @@ ItemTests() {
 
       var layer = new Layer();
       var copy = path.copyTo(layer);
-      equals(() {
-        return layer.children.indexOf(copy) != -1;
-      }, true);
-      equals(() {
-        return project.layers[0].children.indexOf(copy) == -1;
-      }, true);
+      expect(layer.children.indexOf(copy) != -1, true);
+      expect(project.layers[0].children.indexOf(copy) == -1, true);
     });
 
     test('clone()', () {
       var project = paper.project;
       var path = new Path();
       var copy = path.clone();
-      equals(() {
-        return project.activeLayer.children.length;
-      }, 2);
-      equals(() {
-        return path != copy;
-      }, true);
+      expect(project.activeLayer.children.length, 2);
+      expect(path != copy, true);
     });
 
     test('addChild(item)', () {
       var project = paper.project;
       var path = new Path();
       project.activeLayer.addChild(path);
-      equals(() {
-        return project.activeLayer.children.length;
-      },  1);
+      expect(project.activeLayer.children.length,  1);
     });
 
     test('item.parent / item.isChild / item.isParent / item.layer', () {
@@ -72,46 +56,24 @@ ItemTests() {
       var secondDoc = new Project();
       var path = new Path();
       project.activeLayer.addChild(path);
-      equals(() {
-        return project.activeLayer.children.indexOf(path) != -1;
-      }, true);
-        equals(() {
-        return path.layer == project.activeLayer;
-      }, true);
+      expect(project.activeLayer.children.indexOf(path) != -1, true);
+      expect(path.layer == project.activeLayer, true);
       secondDoc.activeLayer.addChild(path);
-      equals(() {
-        return project.activeLayer.isChild(path);
-      }, false);
-      equals(() {
-        return path.layer == secondDoc.activeLayer;
-      }, true);
-      equals(() {
-        return path.isParent(project.activeLayer);
-      }, false);
-      equals(() {
-        return secondDoc.activeLayer.isChild(path);
-      }, true);
-      equals(() {
-        return path.isParent(secondDoc.activeLayer);
-      }, true);
-      equals(() {
-        return project.activeLayer.children.indexOf(path) == -1;
-      }, true);
-      equals(() {
-        return secondDoc.activeLayer.children.indexOf(path) == 0;
-      }, true);
+      expect(project.activeLayer.isChild(path), false);
+      expect(path.layer == secondDoc.activeLayer, true);
+      expect(path.isParent(project.activeLayer), false);
+      expect(secondDoc.activeLayer.isChild(path), true);
+      expect(path.isParent(secondDoc.activeLayer), true);
+      expect(project.activeLayer.children.indexOf(path) == -1, true);
+      expect(secondDoc.activeLayer.children.indexOf(path) == 0, true);
     });
 
     test('item.lastChild / item.firstChild', () {
       var project = paper.project;
       var path = new Path();
       var secondPath = new Path();
-      equals(() {
-        return project.activeLayer.firstChild == path;
-      }, true);
-      equals(() {
-        return project.activeLayer.lastChild == secondPath;
-      }, true);
+      expect(project.activeLayer.firstChild == path, true);
+      expect(project.activeLayer.lastChild == secondPath, true);
     });
 
     test('insertChild(0, item)', () {
@@ -119,9 +81,7 @@ ItemTests() {
       var path = new Path();
       var secondPath = new Path();
       project.activeLayer.insertChild(0, secondPath);
-      equals(() {
-        return secondPath.index < path.index;
-      }, true);
+      expect(secondPath.index < path.index, true);
     });
 
     test('insertAbove(item)', () {
@@ -129,48 +89,30 @@ ItemTests() {
       var path = new Path();
       var secondPath = new Path();
       path.insertAbove(secondPath);
-      equals(() {
-        return project.activeLayer.lastChild == path;
-      }, true);
+      expect(project.activeLayer.lastChild == path, true);
     });
 
     test('insertBelow(item)', () {
       var project = paper.project;
       var firstPath = new Path();
       var secondPath = new Path();
-      equals(() {
-        return secondPath.index > firstPath.index;
-      }, true);
+      expect(secondPath.index > firstPath.index, true);
       secondPath.insertBelow(firstPath);
-      equals(() {
-        return secondPath.index < firstPath.index;
-      }, true);
+      expect(secondPath.index < firstPath.index, true);
     });
 
     test('isDescendant(item) / isAncestor(item)', () {
       var project = paper.project;
       var path = new Path();
-      equals(() {
-        return path.isDescendant(project.activeLayer);
-      }, true);
-      equals(() {
-        return project.activeLayer.isDescendant(path);
-      }, false);
-      equals(() {
-        return path.isAncestor(project.activeLayer);
-      }, false);
-      equals(() {
-        return project.activeLayer.isAncestor(path);
-      }, true);
+      expect(path.isDescendant(project.activeLayer), true);
+      expect(project.activeLayer.isDescendant(path), false);
+      expect(path.isAncestor(project.activeLayer), false);
+      expect(project.activeLayer.isAncestor(path), true);
 
       // an item can't be its own descendant:
-      equals(() {
-        return project.activeLayer.isDescendant(project.activeLayer);
-      }, false);
+      expect(project.activeLayer.isDescendant(project.activeLayer), false);
       // an item can't be its own ancestor:
-      equals(() {
-        return project.activeLayer.isAncestor(project.activeLayer);
-      }, false);
+      expect(project.activeLayer.isAncestor(project.activeLayer), false);
     });
 
     test('isGroupedWith', () {
@@ -180,50 +122,26 @@ ItemTests() {
       var group = new Group([path]);
       var secondGroup = new Group([secondPath]);
 
-      equals(() {
-        return path.isGroupedWith(secondPath);
-      }, false);
+      expect(path.isGroupedWith(secondPath), false);
       secondGroup.addChild(path);
-      equals(() {
-        return path.isGroupedWith(secondPath);
-      }, true);
-      equals(() {
-        return path.isGroupedWith(group);
-      }, false);
-      equals(() {
-        return path.isDescendant(secondGroup);
-      }, true);
-      equals(() {
-        return secondGroup.isDescendant(path);
-      }, false);
-      equals(() {
-        return secondGroup.isDescendant(secondGroup);
-      }, false);
-      equals(() {
-        return path.isGroupedWith(secondGroup);
-      }, false);
+      expect(path.isGroupedWith(secondPath), true);
+      expect(path.isGroupedWith(group), false);
+      expect(path.isDescendant(secondGroup), true);
+      expect(secondGroup.isDescendant(path), false);
+      expect(secondGroup.isDescendant(secondGroup), false);
+      expect(path.isGroupedWith(secondGroup), false);
       paper.project.activeLayer.addChild(path);
-      equals(() {
-        return path.isGroupedWith(secondPath);
-      }, false);
+      expect(path.isGroupedWith(secondPath), false);
       paper.project.activeLayer.addChild(secondPath);
-      equals(() {
-        return path.isGroupedWith(secondPath);
-      }, false);
+      expect(path.isGroupedWith(secondPath), false);
     });
 
     test('getPreviousSibling() / getNextSibling()', () {
       var firstPath = new Path();
       var secondPath = new Path();
-      equals(() {
-        return firstPath.nextSibling == secondPath;
-      }, true);
-      equals(() {
-        return secondPath.previousSibling == firstPath;
-      }, true);
-      equals(() {
-        return secondPath.nextSibling == null;
-      }, true);
+      expect(firstPath.nextSibling == secondPath, true);
+      expect(secondPath.previousSibling == firstPath, true);
+      expect(secondPath.nextSibling == null, true);
     });
 
     test('reverseChildren()', () {
@@ -231,19 +149,11 @@ ItemTests() {
       var path = new Path();
       var secondPath = new Path();
       var thirdPath = new Path();
-      equals(() {
-        return project.activeLayer.firstChild == path;
-      }, true);
+      expect(project.activeLayer.firstChild == path, true);
       project.activeLayer.reverseChildren();
-      equals(() {
-        return project.activeLayer.firstChild == path;
-      }, false);
-      equals(() {
-        return project.activeLayer.firstChild == thirdPath;
-      }, true);
-      equals(() {
-        return project.activeLayer.lastChild == path;
-      }, true);
+      expect(project.activeLayer.firstChild == path, false);
+      expect(project.activeLayer.firstChild == thirdPath, true);
+      expect(project.activeLayer.lastChild == path, true);
     });
 
     test('Check item#project when moving items across projects', () {
@@ -253,19 +163,13 @@ ItemTests() {
       var group = new Group();
       group.addChild(new Path());
 
-      equals(() {
-        return path.project == doc1;
-      }, true);
+      expect(path.project == doc1, true);
       var doc2 = new Project();
       doc2.activeLayer.addChild(path);
-      equals(() {
-        return path.project == doc2;
-      }, true);
+      expect(path.project == doc2, true);
 
       doc2.activeLayer.addChild(group);
-      equals(() {
-        return group.children[0].project == doc2;
-      }, true);
+      expect(group.children[0].project == doc2, true);
     });
 
     test('group.selected', () {
@@ -273,57 +177,37 @@ ItemTests() {
       var path2 = new Path([0, 0]);
       var group = new Group([path, path2]);
       path.selected = true;
-      equals(() {
-        return group.selected;
-      }, true);
+      expect(group.selected, true);
 
       path.selected = false;
-      equals(() {
-        return group.selected;
-      }, false);
+      expect(group.selected, false);
 
       group.selected = true;
-      equals(() {
-        return path.selected;
-      }, true);
-      equals(() {
-        return path2.selected;
-      }, true);
+      expect(path.selected, true);
+      expect(path2.selected, true);
 
       group.selected = false;
-      equals(() {
-        return path.selected;
-      }, false);
-      equals(() {
-        return path2.selected;
-      }, false);
+      expect(path.selected, false);
+      expect(path2.selected, false);
     });
 
     test('Check parent children object for named item', () {
       var path = new Path();
       path.name = 'test';
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path, true);
 
       var path2 = new Path();
       path2.name = 'test';
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path2;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path2, true);
 
       path2.remove();
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path, true);
 
       path.remove();
 
-      equals(() {
-        return !paper.project.activeLayer.children['test'];
-      }, true);
+      expect(!paper.project.activeLayer.children['test'], true);
     });
 
     test('Named child access 1', () {
@@ -335,9 +219,7 @@ ItemTests() {
 
       path.remove();
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path2;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path2, true);
     });
 
     test('Named child access 2', () {
@@ -349,23 +231,15 @@ ItemTests() {
 
       path.remove();
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path2;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path2, true);
 
-      equals(() {
-        return paper.project.activeLayer._namedChildren['test'].length == 1;
-      }, true);
+      expect(paper.project.activeLayer._namedChildren['test'].length == 1, true);
 
       path2.remove();
 
-      equals(() {
-        return !paper.project.activeLayer._namedChildren['test'];
-      }, true);
+      expect(!paper.project.activeLayer._namedChildren['test'], true);
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] === undefined;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] === undefined, true);
     });
 
     test('Named child access 3', () {
@@ -379,44 +253,26 @@ ItemTests() {
 
       group.addChild(path2);
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path, true);
 
       // TODO: Tests should not access internal properties
-      equals(() {
-        return paper.project.activeLayer._namedChildren['test'].length;
-      }, 1);
+      expect(paper.project.activeLayer._namedChildren['test'].length, 1);
 
-      equals(() {
-        return group.children['test'] == path2;
-      }, true);
+      expect(group.children['test'] == path2, true);
 
-      equals(() {
-        return group._namedChildren['test'].length == 1;
-      }, true);
+      expect(group._namedChildren['test'].length == 1, true);
 
-      equals(() {
-        return paper.project.activeLayer._namedChildren['test'][0] == path;
-      }, true);
+      expect(paper.project.activeLayer._namedChildren['test'][0] == path, true);
 
       paper.project.activeLayer.appendTop(path2);
 
-      equals(() {
-        return group.children['test'] == null;
-      }, true);
+      expect(group.children['test'] == null, true);
 
-      equals(() {
-        return group._namedChildren['test'] === undefined;
-      }, true);
+      expect(group._namedChildren['test'] === undefined, true);
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path2;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path2, true);
 
-      equals(() {
-        return paper.project.activeLayer._namedChildren['test'].length;
-      }, 2);
+      expect(paper.project.activeLayer._namedChildren['test'].length, 2);
     });
 
     test('Setting name of child back to null', () {
@@ -426,21 +282,15 @@ ItemTests() {
       var path2 = new Path();
       path2.name = 'test';
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path2;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path2, true);
 
       path2.name = null;
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] == path;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] == path, true);
 
       path.name = null;
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] === undefined;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] === undefined, true);
     });
 
     test('Renaming item', () {
@@ -449,19 +299,15 @@ ItemTests() {
 
       path.name = 'test2';
 
-      equals(() {
-        return paper.project.activeLayer.children['test'] === undefined;
-      }, true);
+      expect(paper.project.activeLayer.children['test'] === undefined, true);
 
-      equals(() {
-        return paper.project.activeLayer.children['test2'] == path;
-      }, true);
+      expect(paper.project.activeLayer.children['test2'] == path, true);
     });
 
     test('Changing item#position.x', () {
       var path = new Path.Circle(new Point(50, 50), 50);
       path.position.x += 5;
-      equals(path.position.toString(), '{ x: 55, y: 50 }', 'path.position.x += 5');
+      expect(path.position.toString(), '{ x: 55, y: 50 }', 'path.position.x += 5');
     });
 
     test('Naming a removed item', () {
@@ -486,7 +332,7 @@ ItemTests() {
       var description = 'Cloning a linked size should not throw an error';
       if (error)
         description += ': ' + error;
-      equals(error == null, true, description);
+      expect(error == null, true, description);
     });
   });
 }
