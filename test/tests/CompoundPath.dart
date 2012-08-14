3830bd51e308b76bfd3ad1eeda66720023e04859
Christopher Best <bestchris@gmail.com>
2012-08-14 14:12:25 -0400

    put tests in group in top level fn

--- test/tests/CompoundPath.dart
+++ test/tests/CompoundPath.dart
@@ -14,59 +14,62 @@
  * All rights reserved.
  */
 
+CompoundPathTests() {
+  group("CompoundPath Tests") () {
+    test('moveTo / lineTo', () {
+      var path = new CompoundPath();
 
-test('moveTo / lineTo', () {
-  var path = new CompoundPath();
+      var lists = [
+        [new Point(279, 151), new Point(149, 151), new Point(149, 281), new Point(279, 281)],
+        [new Point(319, 321), new Point(109, 321), new Point(109, 111), new Point(319, 111)]
+      ];
 
-  var lists = [
-    [new Point(279, 151), new Point(149, 151), new Point(149, 281), new Point(279, 281)],
-    [new Point(319, 321), new Point(109, 321), new Point(109, 111), new Point(319, 111)]
-  ];
+      for (var i = 0; i < lists.length; i++) {
+        var list = lists[i];
+        for (var j = 0; j < list.length; j++) {
+          path[j == 0 ? 'moveTo' : 'lineTo'](list[j]);
+        }
+      }
 
-  for (var i = 0; i < lists.length; i++) {
-    var list = lists[i];
-    for (var j = 0; j < list.length; j++) {
-      path[j == 0 ? 'moveTo' : 'lineTo'](list[j]);
-    }
+      path.fillColor = 'black';
+
+      equals(() {
+        return path.children.length;
+      }, 2);
+    });
+
+    test('clockwise', () {
+      var path1 = new Path.Rectangle([200, 200], [100, 100]);
+      var path2 = new Path.Rectangle([50, 50], [200, 200]);
+      var path3 = new Path.Rectangle([0, 0], [400, 400]);
+
+      equals(() {
+        return path1.clockwise;
+      }, true);
+      equals(() {
+        return path2.clockwise;
+      }, true);
+      equals(() {
+        return path3.clockwise;
+      }, true);
+
+      var compound = new CompoundPath(path1, path2, path3);
+
+      equals(() {
+        return compound.lastChild == path3;
+      }, true);
+      equals(() {
+        return compound.firstChild == path1;
+      }, true);
+      equals(() {
+        return path1.clockwise;
+      }, true);
+      equals(() {
+        return path2.clockwise;
+      }, false);
+      equals(() {
+        return path3.clockwise;
+      }, false);
+    });
   }
-
-  path.fillColor = 'black';
-
-  equals(() {
-    return path.children.length;
-  }, 2);
-});
-
-test('clockwise', () {
-  var path1 = new Path.Rectangle([200, 200], [100, 100]);
-  var path2 = new Path.Rectangle([50, 50], [200, 200]);
-  var path3 = new Path.Rectangle([0, 0], [400, 400]);
-
-  equals(() {
-    return path1.clockwise;
-  }, true);
-  equals(() {
-    return path2.clockwise;
-  }, true);
-  equals(() {
-    return path3.clockwise;
-  }, true);
-
-  var compound = new CompoundPath(path1, path2, path3);
-
-  equals(() {
-    return compound.lastChild == path3;
-  }, true);
-  equals(() {
-    return compound.firstChild == path1;
-  }, true);
-  equals(() {
-    return path1.clockwise;
-  }, true);
-  equals(() {
-    return path2.clockwise;
-  }, false);
-  equals(() {
-    return path3.clockwise;
-  }, false);
-})
+}
