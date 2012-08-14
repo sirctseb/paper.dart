4180339f725e7ea55e3eefecfba9e61199ee7659
Christopher Best <bestchris@gmail.com>
2012-08-14 13:48:00 -0400

    tabs to spaces

--- src/item/ChangeFlag.dart
+++ src/item/ChangeFlag.dart
@@ -15,34 +15,34 @@
  */
 
 var ChangeFlag = {
-	// Anything affecting the appearance of an item, including GEOMETRY,
-	// STROKE, STYLE and ATTRIBUTE (except for the invisible ones: locked, name)
-	APPEARANCE: 1,
-	// Change in item hierarchy
-	HIERARCHY: 2,
-	// Item geometry (path, bounds)
-	GEOMETRY: 4,
-	// Stroke geometry (excluding color)
-	STROKE: 8,
-	// Fill style or stroke color / dash
-	STYLE: 16,
-	// Item attributes: visible, blendMode, locked, name, opacity, clipMask ...
-	ATTRIBUTE: 32,
-	// Text content
-	CONTENT: 64,
-	// Raster pixels
-	PIXELS: 128,
-	// Clipping in one of the child items
-	CLIPPING: 256
+  // Anything affecting the appearance of an item, including GEOMETRY,
+  // STROKE, STYLE and ATTRIBUTE (except for the invisible ones: locked, name)
+  APPEARANCE: 1,
+  // Change in item hierarchy
+  HIERARCHY: 2,
+  // Item geometry (path, bounds)
+  GEOMETRY: 4,
+  // Stroke geometry (excluding color)
+  STROKE: 8,
+  // Fill style or stroke color / dash
+  STYLE: 16,
+  // Item attributes: visible, blendMode, locked, name, opacity, clipMask ...
+  ATTRIBUTE: 32,
+  // Text content
+  CONTENT: 64,
+  // Raster pixels
+  PIXELS: 128,
+  // Clipping in one of the child items
+  CLIPPING: 256
 };
 
 // Shortcuts to often used ChangeFlag values including APPEARANCE
 var Change = {
-	HIERARCHY: ChangeFlag.HIERARCHY | ChangeFlag.APPEARANCE,
-	GEOMETRY: ChangeFlag.GEOMETRY | ChangeFlag.APPEARANCE,
-	STROKE: ChangeFlag.STROKE | ChangeFlag.STYLE | ChangeFlag.APPEARANCE,
-	STYLE: ChangeFlag.STYLE | ChangeFlag.APPEARANCE,
-	ATTRIBUTE: ChangeFlag.ATTRIBUTE | ChangeFlag.APPEARANCE,
-	CONTENT: ChangeFlag.CONTENT | ChangeFlag.APPEARANCE,
-	PIXELS: ChangeFlag.PIXELS | ChangeFlag.APPEARANCE
+  HIERARCHY: ChangeFlag.HIERARCHY | ChangeFlag.APPEARANCE,
+  GEOMETRY: ChangeFlag.GEOMETRY | ChangeFlag.APPEARANCE,
+  STROKE: ChangeFlag.STROKE | ChangeFlag.STYLE | ChangeFlag.APPEARANCE,
+  STYLE: ChangeFlag.STYLE | ChangeFlag.APPEARANCE,
+  ATTRIBUTE: ChangeFlag.ATTRIBUTE | ChangeFlag.APPEARANCE,
+  CONTENT: ChangeFlag.CONTENT | ChangeFlag.APPEARANCE,
+  PIXELS: ChangeFlag.PIXELS | ChangeFlag.APPEARANCE
 };
