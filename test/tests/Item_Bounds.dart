7fc1c5df3353f49864e45c73b76e1546b8bb3ed6
Christopher Best <bestchris@gmail.com>
2012-08-14 15:19:40 -0400

    put tests in group in top level fn

--- test/tests/Item_Bounds.dart
+++ test/tests/Item_Bounds.dart
@@ -14,40 +14,42 @@
  * All rights reserved.
  */
 
-module('Item Bounds');
+ItemBoundsTests() {
+  group("Item Bounds Tests", () {
+    test('item.bounds caching', function() {
+      var circle = new Path.Circle(new Point(100, 100), 50);
+      var rectangle = new Path.Rectangle(new Point(75, 75), new Point(175, 175));
+      var group = new Group([circle, rectangle]);
+      compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds');
+      rectangle.remove();
+      equals(function() {
+        return group.children.length;
+      }, 1);
+      compareRectangles(group.bounds, { x: 50, y: 50, width: 100, height: 100 }, 'group.bounds without rectangle');
+      group.addChild(rectangle);
+      equals(function() {
+        return group.children.length;
+      }, 2);
+      compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds with rectangle');
+      circle.remove();
+      equals(function() {
+        return group.children.length;
+      }, 1);
+      compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds without circle');
+      group.addChild(circle);
+      equals(function() {
+        return group.children.length;
+      }, 2);
+      compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds with circle');
+    });
 
-test('item.bounds caching', function() {
-  var circle = new Path.Circle(new Point(100, 100), 50);
-  var rectangle = new Path.Rectangle(new Point(75, 75), new Point(175, 175));
-  var group = new Group([circle, rectangle]);
-  compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds');
-  rectangle.remove();
-  equals(function() {
-    return group.children.length;
-  }, 1);
-  compareRectangles(group.bounds, { x: 50, y: 50, width: 100, height: 100 }, 'group.bounds without rectangle');
-  group.addChild(rectangle);
-  equals(function() {
-    return group.children.length;
-  }, 2);
-  compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds with rectangle');
-  circle.remove();
-  equals(function() {
-    return group.children.length;
-  }, 1);
-  compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds without circle');
-  group.addChild(circle);
-  equals(function() {
-    return group.children.length;
-  }, 2);
-  compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds with circle');
-});
-
-test('group.bounds when group contains empty group', function() {
-  var group = new Group();
-  var rectangle = new Path.Rectangle(new Point(75, 75), new Point(175, 175));
-  group.addChild(rectangle);
-  compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds without empty group');
-  group.addChild(new Group());
-  compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds with empty group');
-});
+    test('group.bounds when group contains empty group', function() {
+      var group = new Group();
+      var rectangle = new Path.Rectangle(new Point(75, 75), new Point(175, 175));
+      group.addChild(rectangle);
+      compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds without empty group');
+      group.addChild(new Group());
+      compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds with empty group');
+    });
+  });
+}
