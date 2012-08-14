4e24f61905797384c64b6635b86edb1815faa8aa
Christopher Best <bestchris@gmail.com>
2012-08-14 13:53:03 -0400

    ported change flags

--- src/item/ChangeFlag.dart
+++ src/item/ChangeFlag.dart
@@ -14,35 +14,35 @@
  * All rights reserved.
  */
 
-var ChangeFlag = {
+class ChangeFlag {
   // Anything affecting the appearance of an item, including GEOMETRY,
   // STROKE, STYLE and ATTRIBUTE (except for the invisible ones: locked, name)
-  APPEARANCE: 1,
+  static final int APPEARANCE = 1;
   // Change in item hierarchy
-  HIERARCHY: 2,
+  static final int HIERARCHY = 2;
   // Item geometry (path, bounds)
-  GEOMETRY: 4,
+  static final int GEOMETRY = 4;
   // Stroke geometry (excluding color)
-  STROKE: 8,
+  static final int STROKE = 8;
   // Fill style or stroke color / dash
-  STYLE: 16,
+  static final int STYLE = 16;
   // Item attributes: visible, blendMode, locked, name, opacity, clipMask ...
-  ATTRIBUTE: 32,
+  static final int ATTRIBUTE = 32;
   // Text content
-  CONTENT: 64,
+  static final int CONTENT = 64;
   // Raster pixels
-  PIXELS: 128,
+  static final int PIXELS = 128;
   // Clipping in one of the child items
-  CLIPPING: 256
-};
+  static final int CLIPPING = 256;
+}
 
 // Shortcuts to often used ChangeFlag values including APPEARANCE
-var Change = {
-  HIERARCHY: ChangeFlag.HIERARCHY | ChangeFlag.APPEARANCE,
-  GEOMETRY: ChangeFlag.GEOMETRY | ChangeFlag.APPEARANCE,
-  STROKE: ChangeFlag.STROKE | ChangeFlag.STYLE | ChangeFlag.APPEARANCE,
-  STYLE: ChangeFlag.STYLE | ChangeFlag.APPEARANCE,
-  ATTRIBUTE: ChangeFlag.ATTRIBUTE | ChangeFlag.APPEARANCE,
-  CONTENT: ChangeFlag.CONTENT | ChangeFlag.APPEARANCE,
-  PIXELS: ChangeFlag.PIXELS | ChangeFlag.APPEARANCE
-};
+class Change {
+  static final int HIERARCHY = ChangeFlag.HIERARCHY | ChangeFlag.APPEARANCE;
+  static final int GEOMETRY = ChangeFlag.GEOMETRY | ChangeFlag.APPEARANCE;
+  static final int STROKE = ChangeFlag.STROKE | ChangeFlag.STYLE | ChangeFlag.APPEARANCE;
+  static final int STYLE = ChangeFlag.STYLE | ChangeFlag.APPEARANCE;
+  static final int ATTRIBUTE = ChangeFlag.ATTRIBUTE | ChangeFlag.APPEARANCE;
+  static final int CONTENT = ChangeFlag.CONTENT | ChangeFlag.APPEARANCE;
+  static final int PIXELS = ChangeFlag.PIXELS | ChangeFlag.APPEARANCE;
+}
