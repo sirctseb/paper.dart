982a37a81cb8c5dd62b3f62c6f9bb6d0b559ff00
Christopher Best <bestchris@gmail.com>
2012-08-14 15:21:06 -0400

    fixed map literal keys and port tests

--- test/tests/Item_Bounds.dart
+++ test/tests/Item_Bounds.dart
@@ -20,36 +20,28 @@ ItemBoundsTests() {
       var circle = new Path.Circle(new Point(100, 100), 50);
       var rectangle = new Path.Rectangle(new Point(75, 75), new Point(175, 175));
       var group = new Group([circle, rectangle]);
-      compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds');
+      compareRectangles(group.bounds, { "x": 50, "y": 50, "width": 125, "height": 125 }, 'group.bounds');
       rectangle.remove();
-      equals(() {
-        return group.children.length;
-      }, 1);
-      compareRectangles(group.bounds, { x: 50, y: 50, width: 100, height: 100 }, 'group.bounds without rectangle');
+      expect(group.children.length, 1);
+      compareRectangles(group.bounds, { "x": 50, "y": 50, "width": 100, "height": 100 }, 'group.bounds without rectangle');
       group.addChild(rectangle);
-      equals(() {
-        return group.children.length;
-      }, 2);
-      compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds with rectangle');
+      expect(group.children.length, 2);
+      compareRectangles(group.bounds, { "x": 50, "y": 50, "width": 125, "height": 125 }, 'group.bounds with rectangle');
       circle.remove();
-      equals(() {
-        return group.children.length;
-      }, 1);
-      compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds without circle');
+      expect(group.children.length, 1);
+      compareRectangles(group.bounds, { "x": 75, "y": 75, "width": 100, "height": 100 }, 'group.bounds without circle');
       group.addChild(circle);
-      equals(() {
-        return group.children.length;
-      }, 2);
-      compareRectangles(group.bounds, { x: 50, y: 50, width: 125, height: 125 }, 'group.bounds with circle');
+      expect(group.children.length, 2);
+      compareRectangles(group.bounds, { "x": 50, "y": 50, "width": 125, "height": 125 }, 'group.bounds with circle');
     });
 
     test('group.bounds when group contains empty group', () {
       var group = new Group();
       var rectangle = new Path.Rectangle(new Point(75, 75), new Point(175, 175));
       group.addChild(rectangle);
-      compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds without empty group');
+      compareRectangles(group.bounds, { "x": 75, "y": 75, "width": 100, "height": 100 }, 'group.bounds without empty group');
       group.addChild(new Group());
-      compareRectangles(group.bounds, { x: 75, y: 75, width: 100, height: 100 }, 'group.bounds with empty group');
+      compareRectangles(group.bounds, { "x": 75, "y": 75, "width": 100, "height": 100 }, 'group.bounds with empty group');
     });
   });
 }
