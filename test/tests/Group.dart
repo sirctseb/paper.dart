8887f0259d926a4ef8099b09258c76fde9564ca9
Christopher Best <bestchris@gmail.com>
2012-08-14 14:20:55 -0400

    ported tests

--- test/tests/Group.dart
+++ test/tests/Group.dart
@@ -18,40 +18,34 @@ GroupTests() {
   group("Group Tests", () {
     test('new Group()', () {
       var group = new Group();
-      equals(() {
-        return paper.project.activeLayer.children[0] == group;
-      }, true);
+      expect(paper.project.activeLayer.children[0] == group);
     });
 
     test('new Group([item])', () {
       var path = new Path();
       var group = new Group([path]);
-      equals(() {
-        return paper.project.activeLayer.children.length;
-      }, 1);
-      equals(() {
-        return group.children[0] == path;
-      }, true);
+      expect(paper.project.activeLayer.children.length, 1);
+      expect(group.children[0] == path);
     });
 
     test('Group bounds', () {
       paper.project.currentStyle = {
-        strokeWidth: 5,
-        strokeColor: 'black'
+        "strokeWidth": 5,
+        "strokeColor": 'black'
       };
 
       var path = new Path.Circle([150, 150], 60);
       var secondPath = new Path.Circle([175, 175], 85);
       var group = new Group([path, secondPath]);
-      compareRectangles(group.bounds, { x: 90, y: 90, width: 170, height: 170 }, 'group.bounds');
-      compareRectangles(group.strokeBounds, { x: 87.5, y: 87.5, width: 175, height: 175 }, 'group.strokeBounds');
+      compareRectangles(group.bounds, { "x": 90, "y": 90, "width": 170, "height": 170 }, 'group.bounds');
+      compareRectangles(group.strokeBounds, { "x": 87.5, "y": 87.5, "width": 175, "height": 175 }, 'group.strokeBounds');
 
       group.rotate(20);
-      compareRectangles(group.bounds, { x: 89.97681, y: 82.94095, width: 170.04639, height: 177.08224 }, 'rotated group.bounds');
-      compareRectangles(group.strokeBounds, { x: 87.47681, y: 80.44095, width: 175.04639, height: 182.08224 }, 'rotated group.strokeBounds');
+      compareRectangles(group.bounds, { "x": 89.97681, "y": 82.94095, "width": 170.04639, "height": 177.08224 }, 'rotated group.bounds');
+      compareRectangles(group.strokeBounds, { "x": 87.47681, "y": 80.44095, "width": 175.04639, "height": 182.08224 }, 'rotated group.strokeBounds');
       group.rotate(20, new Point(50, 50));
-      compareRectangles(group.bounds, { x: 39.70692, y: 114.99196, width: 170.00412, height: 180.22401 }, 'rotated group.bounds');
-      compareRectangles(group.strokeBounds, { x: 37.20692, y: 112.49196, width: 175.00412, height: 185.22401 }, 'rotated group.strokeBounds');
+      compareRectangles(group.bounds, { "x": 39.70692, "y": 114.99196, "width": 170.00412, "height": 180.22401 }, 'rotated group.bounds');
+      compareRectangles(group.strokeBounds, { "x": 37.20692, "y": 112.49196, "width": 175.00412, "height": 185.22401 }, 'rotated group.strokeBounds');
     });
   });
 }
