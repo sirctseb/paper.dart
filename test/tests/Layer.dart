267f2172198fce48722da7d389b71f5049048c36
Christopher Best <bestchris@gmail.com>
2012-08-14 21:01:28 -0400

    ported tests

--- test/tests/Layer.dart
+++ test/tests/Layer.dart
@@ -20,39 +20,23 @@ LayerTests() {
       var project = paper.project;
       var firstLayer = project.activeLayer;
       var secondLayer = new Layer();
-      equals(() {
-        return secondLayer.previousSibling == firstLayer;
-        }, true);
-      equals(() {
-        return secondLayer.nextSibling == null;
-        }, true);
+      expect(secondLayer.previousSibling == firstLayer, true);
+      expect(secondLayer.nextSibling == null, true);
 
       // Move another layer into secondLayer and check nextSibling /
       // previousSibling:
       var path = new Path();
       var thirdLayer = new Layer();
       secondLayer.insertChild(0, thirdLayer);
-      equals(() {
-        return secondLayer.children.length;
-        }, 2);
-      equals(() {
-        return thirdLayer.nextSibling == path;
-        }, true);
+      expect(secondLayer.children.length, 2);
+      expect(thirdLayer.nextSibling == path, true);
       secondLayer.addChild(thirdLayer);
-      equals(() {
-        return thirdLayer.nextSibling == null;
-        }, true);
-      equals(() {
-        return thirdLayer.previousSibling == path;
-        }, true);
-      equals(() {
-        return project.layers.length == 2;
-        }, true);
+      expect(thirdLayer.nextSibling == null, true);
+      expect(thirdLayer.previousSibling == path, true);
+      expect(project.layers.length == 2, true);
 
       firstLayer.addChild(secondLayer);
-      equals(() {
-        return project.layers.length == 1;
-        }, true);
+      expect(project.layers.length == 1, true);
     });
 
     test('insertabove / insertbelow', () {
@@ -60,28 +44,18 @@ LayerTests() {
       var firstlayer = project.activelayer;
       var secondlayer = new layer();
       secondlayer.insertbelow(firstlayer);
-      equals(() {
-        return secondlayer.previoussibling == null;
-        }, true);
-      equals(() {
-        return secondlayer.nextsibling == firstlayer;
-        }, true);
+      expect(secondlayer.previoussibling == null, true);
+      expect(secondlayer.nextsibling == firstlayer, true);
 
       var path = new path();
       firstlayer.addchild(path);
 
       // move the layer above the path, inside the firstlayer:
       secondlayer.insertabove(path);
-      equals(() {
-        return secondlayer.nextsibling == path;
-        }, true);
-      equals(() {
-        return secondlayer.parent == firstlayer;
-        }, true);
+      expect(secondlayer.nextsibling == path, true);
+      expect(secondlayer.parent == firstlayer, true);
       // there should now only be one layer left:
-      equals(() {
-        return project.layers.length;
-        }, 1);
+      expect(project.layers.length, 1);
     });
 
     test('addchild / appendbottom / nesting', () {
@@ -89,31 +63,19 @@ LayerTests() {
       var firstlayer = project.activelayer;
       var secondlayer = new layer();
       // there should be two layers now in project.layers
-      equals(() {
-        return project.layers.length;
-        }, 2);
+      expect(project.layers.length, 2);
       firstlayer.addchild(secondlayer);
-      equals(() {
-        return secondlayer.parent == firstlayer;
-        }, true);
+      expect(secondlayer.parent == firstlayer, true);
       // there should only be the firslayer now in project.layers
-      equals(() {
-        return project.layers.length;
-        }, 1);
-      equals(() {
-        return project.layers[0] == firstlayer;
-        }, true);
+      expect(project.layers.length, 1);
+      expect(project.layers[0] == firstlayer, true);
       // now move secondlayer bellow the first again, in which case it should
       // reappear in project.layers
       secondlayer.insertbelow(firstlayer);
       // there should be two layers now in project.layers again now
-      equals(() {
-        return project.layers.length;
-        }, 2);
-      equals(() {
-        return project.layers[0] == secondlayer
-        && project.layers[1] == firstlayer;
-        }, true);
+      expect(project.layers.length, 2);
+      expect(project.layers[0] == secondlayer
+        && project.layers[1] == firstlayer, true);
     });
   });
 }
