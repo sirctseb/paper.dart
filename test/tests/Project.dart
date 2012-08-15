7ca0de6189cadd62cb61e17956a801e1fae5c562
Christopher Best <bestchris@gmail.com>
2012-08-15 09:20:52 -0400

    tabs to spaces; removed function keywords

--- test/tests/Project.dart
+++ test/tests/Project.dart
@@ -16,15 +16,15 @@
 
 module('Project');
 
-test('activate()', function() {
-	var project = new Project();
-	var secondDoc = new Project();
-	project.activate();
-	var path = new Path();
-	equals(function() {
-		return project.activeLayer.children[0] == path;
-	}, true);
-	equals(function() {
-		return secondDoc.activeLayer.children.length == 0;
-	}, true);
+test('activate()', () {
+  var project = new Project();
+  var secondDoc = new Project();
+  project.activate();
+  var path = new Path();
+  equals(() {
+    return project.activeLayer.children[0] == path;
+  }, true);
+  equals(() {
+    return secondDoc.activeLayer.children.length == 0;
+  }, true);
 });
