67a7d572f6f4ca77bb39e45c96ab2ae6fc94b1c8
Christopher Best <bestchris@gmail.com>
2012-08-14 21:17:38 -0400

    added copyright stuff for consistency

--- test/tests/PathStyle.dart
+++ test/tests/PathStyle.dart
@@ -1,4 +1,18 @@
-module('Path Style');
+/*
+ * Paper.js
+ *
+ * This file is part of Paper.js, a JavaScript Vector Graphics Library,
+ * based on Scriptographer.org and designed to be largely API compatible.
+ * http://paperjs.org/
+ * http://scriptographer.org/
+ *
+ * Copyright (c) 2011, Juerg Lehni & Jonathan Puckey
+ * http://lehni.org/ & http://jonathanpuckey.com/
+ *
+ * Distributed under the MIT license. See LICENSE file for details.
+ *
+ * All rights reserved.
+ */
 
 test('style defaults', function() {
   var path = new Path();
