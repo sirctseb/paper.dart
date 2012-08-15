1fff5931289eb244fd1686fdb85f26057cf39e4b
Christopher Best <bestchris@gmail.com>
2012-08-14 21:12:45 -0400

    ported tests

--- test/tests/Path.dart
+++ test/tests/Path.dart
@@ -26,10 +26,8 @@ PathTests() {
       path2.add(20, 10);
 
       path.join(path2);
-      equals(path.segments.toString(), '{ point: { x: 0, y: 0 } },{ point: { x: 10, y: 0 } },{ point: { x: 20, y: 10 } }');
-      equals(() {
-        return paper.project.activeLayer.children.length;
-        }, 1);
+      expect(path.segments.toString(), '{ point: { x: 0, y: 0 } },{ point: { x: 10, y: 0 } },{ point: { x: 20, y: 10 } }');
+      expect(paper.project.activeLayer.children.length, 1);
 
       var path = new Path();
       path.add(0, 0);
@@ -39,7 +37,7 @@ PathTests() {
       path2.add(20, 10);
       path2.add(10, 0);
       path.join(path2);
-      equals(path.segments.toString(), '{ point: { x: 0, y: 0 } },{ point: { x: 10, y: 0 } },{ point: { x: 20, y: 10 } }');
+      expect(path.segments.toString(), '{ point: { x: 0, y: 0 } },{ point: { x: 10, y: 0 } },{ point: { x: 20, y: 10 } }');
 
       var path = new Path();
       path.add(0, 0);
@@ -49,7 +47,7 @@ PathTests() {
       path2.add(30, 10);
       path2.add(40, 0);
       path.join(path2);
-      equals(path.segments.toString(), '{ point: { x: 0, y: 0 } },{ point: { x: 10, y: 0 } },{ point: { x: 30, y: 10 } },{ point: { x: 40, y: 0 } }');
+      expect(path.segments.toString(), '{ point: { x: 0, y: 0 } },{ point: { x: 10, y: 0 } },{ point: { x: 30, y: 10 } },{ point: { x: 40, y: 0 } }');
 
       var path = new Path();
       path.add(0, 0);
@@ -63,10 +61,8 @@ PathTests() {
 
       path.join(path2);
 
-      equals(path.segments.toString(), '{ point: { x: 0, y: 0 } },{ point: { x: 10, y: 0 } },{ point: { x: 20, y: 10 } },{ point: { x: 10, y: 5 } }');
-      equals(() {
-        return path.closed;
-        }, true);
+      expect(path.segments.toString(), '{ point: { x: 0, y: 0 } },{ point: { x: 10, y: 0 } },{ point: { x: 20, y: 10 } },{ point: { x: 10, y: 5 } }');
+      expect(path.closed, true);
     });
 
     test('path.remove()', () {
@@ -77,25 +73,17 @@ PathTests() {
       path.add(30, 0);
 
       path.removeSegment(0);
-      equals(() {
-        return path.segments.length;
-        }, 3);
+      expect(path.segments.length, 3);
 
       path.removeSegment(0);
-      equals(() {
-        return path.segments.length;
-        }, 2);
+      expect(path.segments.length, 2);
 
       path.removeSegments(0, 1);
-      equals(() {
-        return path.segments.length;
-        }, 1);
+      expect(path.segments.length, 1);
 
       path.remove();
 
-      equals(() {
-        return paper.project.activeLayer.children.length;
-        }, 0);
+      expect(paper.project.activeLayer.children.length, 0);
     });
 
     test('path.removeSegments()', () {
@@ -106,61 +94,41 @@ PathTests() {
       path.add(30, 0);
 
       path.removeSegments();
-      equals(() {
-        return path.segments.length;
-        }, 0);
+      expect(path.segments.length, 0);
     });
 
     test('Is the path deselected after setting a new list of segments?', () {
       var path = new Path([0, 0]);
       path.selected = true;
-      equals(() {
-        return path.selected;
-        }, true);
-      equals(() {
-        return paper.project.selectedItems.length;
-        }, 1);
+      expect(path.selected, true);
+      expect(paper.project.selectedItems.length, 1);
 
       path.segments = [[0, 10]];
-      equals(() {
-        return path.selected;
-        }, true);
-      equals(() {
-        return paper.project.selectedItems.length;
-        }, 1);
+      expect(path.selected, true);
+      expect(paper.project.selectedItems.length, 1);
     });
 
     test('After setting Path#fullySelected=true on an empty path, subsequent segments should be selected', () {
       var path = new Path();
       path.fullySelected = true;
-      equals(() {
-        return path.fullySelected;
-        }, true);
+      expect(path.fullySelected, true);
       path.add([10, 10]);
-      equals(() {
-        return path.fullySelected;
-        }, true);
-      equals(() {
-        return path.firstSegment.selected;
-        }, true);
+      expect(path.fullySelected, true);
+      expect(path.firstSegment.selected, true);
     });
 
     test('After removing all segments of a fully selected path, it should still be fully selected.', () {
       var path = new Path([10, 20], [30, 40]);
       path.fullySelected = true;
       path.removeSegments();
-      equals(() {
-        return path.fullySelected;
-        }, true);
+      expect(path.fullySelected, true);
     });
 
     test('After removing all segments of a selected path, it should still be selected.', () {
       var path = new Path([10, 20], [30, 40]);
       path.selected = true;
       path.removeSegments();
-      equals(() {
-        return path.selected;
-        }, true);
+      expect(path.selected, true);
     });
 
 
@@ -170,18 +138,12 @@ PathTests() {
         path.add(i * 10, 10);
       };
       path.fullySelected = true;
-      equals(() {
-        return path.selected;
-        }, true);
+      expect(path.selected, true);
 
       path.simplify();
 
-      equals(() {
-        return path.selected;
-        }, true);
-      equals(() {
-        return path.fullySelected;
-        }, true);
+      expect(path.selected, true);
+      expect(path.fullySelected, true);
     });
 
     test('After simplifying a path using #simplify(), the path should stay fullySelected', () {
@@ -190,18 +152,12 @@ PathTests() {
         path.add(i * 10, 10);
       };
       path.fullySelected = true;
-      equals(() {
-        return path.selected;
-        }, true);
+      expect(path.selected, true);
 
       path.simplify();
 
-      equals(() {
-        return path.selected;
-        }, true);
-      equals(() {
-        return path.fullySelected;
-        }, true);
+      expect(path.selected, true);
+      expect(path.fullySelected, true);
     });
 
     test('After cloning a selected item, it should be added to the Project#selectedItems array', () {
@@ -209,9 +165,7 @@ PathTests() {
       path.selected = true;
       var copy = path.clone();
 
-      equals(() {
-        return paper.project.selectedItems.length
-        }, 2);
+      expect(paper.project.selectedItems.length, 2);
     });
 
     test('After simplifying a path using #simplify(), the path should stay selected', () {
@@ -221,9 +175,7 @@ PathTests() {
       };
       path.selected = true;
       path.simplify();
-      equals(() {
-        return path.selected;
-        }, true);
+      expect(path.selected, true);
     });
 
     test('After smoothing a path using #smooth(), the path should stay fullySelected', () {
@@ -233,9 +185,7 @@ PathTests() {
       };
       path.fullySelected = true;
       path.smooth();
-      equals(() {
-        return path.fullySelected;
-        }, true);
+      expect(path.fullySelected, true);
     });
 
     test('After smoothing a path using #smooth(), the path should stay selected', () {
@@ -245,41 +195,35 @@ PathTests() {
       };
       path.selected = true;
       path.smooth();
-      equals(() {
-        return path.selected;
-        }, true);
+      expect(path.selected, true);
     });
 
     test('After selecting a segment, Path#selected should return true', () {
       var path = new Path();
       path.add([10, 10]);
       path.firstSegment.selected = true;
-      equals(() {
-        return path.selected;
-        }, true);
+      expect(path.selected, true);
     });
 
     test('Path#reverse', () {
       var path = new Path.Circle([100, 100], 30);
       path.reverse();
-      equals(path.segments.toString(), '{ point: { x: 100, y: 130 }, handleIn: { x: -16.56854, y: 0 }, handleOut: { x: 16.56854, y: 0 } },{ point: { x: 130, y: 100 }, handleIn: { x: 0, y: 16.56854 }, handleOut: { x: 0, y: -16.56854 } },{ point: { x: 100, y: 70 }, handleIn: { x: 16.56854, y: 0 }, handleOut: { x: -16.56854, y: 0 } },{ point: { x: 70, y: 100 }, handleIn: { x: 0, y: -16.56854 }, handleOut: { x: 0, y: 16.56854 } }');
+      expect(path.segments.toString(), '{ point: { x: 100, y: 130 }, handleIn: { x: -16.56854, y: 0 }, handleOut: { x: 16.56854, y: 0 } },{ point: { x: 130, y: 100 }, handleIn: { x: 0, y: 16.56854 }, handleOut: { x: 0, y: -16.56854 } },{ point: { x: 100, y: 70 }, handleIn: { x: 16.56854, y: 0 }, handleOut: { x: -16.56854, y: 0 } },{ point: { x: 70, y: 100 }, handleIn: { x: 0, y: -16.56854 }, handleOut: { x: 0, y: 16.56854 } }');
     });
 
     test('Path#reverse should adjust segment indices', () {
       var path = new Path([[0, 0], [10, 10], [20, 20]]);
       path.reverse();
-      equals(path.segments[0].index, 0);
-      equals(path.segments[1].index, 1);
-      equals(path.segments[2].index, 2);
+      expect(path.segments[0].index, 0);
+      expect(path.segments[1].index, 1);
+      expect(path.segments[2].index, 2);
     });
 
     test('Path#fullySelected', () {
       var path = new Path.Circle([100, 100], 10);
       path.fullySelected = true;
       path.segments[1].selected = false;
-      equals(() {
-        return path.fullySelected;
-        }, false);
+      expect(path.fullySelected, false);
     });
   });
 }
