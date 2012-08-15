fff2e6b4c5076c7e06ee53e2f396ecfac1e39c2d
Christopher Best <bestchris@gmail.com>
2012-08-15 09:05:57 -0400

    put tests in group in top level fn

--- test/tests/Path_Length.dart
+++ test/tests/Path_Length.dart
@@ -14,27 +14,30 @@
  * All rights reserved.
  */
 
-module('Path Length');
+PathLengthTests() {
+  group("Path Length Tests", () {
 
-test('path.length', () {
-  var path = new Path([
-    new Segment(new Point(121, 334), new Point(-19, 38), new Point(30.7666015625, -61.53369140625)),
-    new Segment(new Point(248, 320), new Point(-42, -74), new Point(42, 74))
-  ]);
+    test('path.length', () {
+      var path = new Path([
+        new Segment(new Point(121, 334), new Point(-19, 38), new Point(30.7666015625, -61.53369140625)),
+        new Segment(new Point(248, 320), new Point(-42, -74), new Point(42, 74))
+      ]);
 
-  var length = path.length;
-  compareNumbers(length, 172.10122680664062);
+      var length = path.length;
+      compareNumbers(length, 172.10122680664062);
 
-  var param = path.curves[0].getParameterAt(length / 4);
-  compareNumbers(param, 0.2255849553116685);
-});
+      var param = path.curves[0].getParameterAt(length / 4);
+      compareNumbers(param, 0.2255849553116685);
+    });
 
-test('curve.getParameter with straight curve', () {
-  var path = new Path();
-  path.moveTo(100, 100);
-  path.lineTo(500, 500);
-  var curve = path.curves[0];
-  var length = curve.length;
-  var t = curve.getParameterAt(length / 3);
-  compareNumbers(t, 0.3869631475722452);
-});
+    test('curve.getParameter with straight curve', () {
+      var path = new Path();
+      path.moveTo(100, 100);
+      path.lineTo(500, 500);
+      var curve = path.curves[0];
+      var length = curve.length;
+      var t = curve.getParameterAt(length / 3);
+      compareNumbers(t, 0.3869631475722452);
+    });
+  });
+}
