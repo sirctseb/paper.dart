c72812fe1b6fc46a35f54dcdf2091908f63bbea2
Christopher Best <bestchris@gmail.com>
2012-08-15 09:00:27 -0400

    ported tests

--- test/tests/Path_Drawing_Commands.dart
+++ test/tests/Path_Drawing_Commands.dart
@@ -21,14 +21,14 @@ TestFunctionName() {
       var path = new Path();
       path.moveTo([50, 50]);
       path.lineTo([100, 100]);
-      equals(path.segments.toString(), '{ point: { x: 50, y: 50 } },{ point: { x: 100, y: 100 } }');
+      expect(path.segments.toString(), '{ point: { x: 50, y: 50 } },{ point: { x: 100, y: 100 } }');
     });
 
     test('path.arcTo(from, through, to);', () {
       var path = new Path();
       path.moveTo([0, 20]);
       path.arcTo([75, 75], [100, 0]);
-      equals(path.segments.toString(), '{ point: { x: 0, y: 20 }, handleOut: { x: -2.62559, y: 23.01251 } },{ point: { x: 30.89325, y: 74.75812 }, handleIn: { x: -21.05455, y: -9.65273 }, handleOut: { x: 21.05455, y: 9.65273 } },{ point: { x: 92.54397, y: 62.42797 }, handleIn: { x: -15.72238, y: 17.00811 }, handleOut: { x: 15.72238, y: -17.00811 } },{ point: { x: 100, y: 0 }, handleIn: { x: 11.27458, y: 20.23247 } }');
+      expect(path.segments.toString(), '{ point: { x: 0, y: 20 }, handleOut: { x: -2.62559, y: 23.01251 } },{ point: { x: 30.89325, y: 74.75812 }, handleIn: { x: -21.05455, y: -9.65273 }, handleOut: { x: 21.05455, y: 9.65273 } },{ point: { x: 92.54397, y: 62.42797 }, handleIn: { x: -15.72238, y: 17.00811 }, handleOut: { x: 15.72238, y: -17.00811 } },{ point: { x: 100, y: 0 }, handleIn: { x: 11.27458, y: 20.23247 } }');
     });
 
     test('path.arcTo(from, through, to); where from, through and to all share the same y position and through lies in between from and to', () {
@@ -37,7 +37,7 @@ TestFunctionName() {
 
       path.add([40, 75]);
       path.arcTo([50, 75], [100, 75]);
-      equals(path.lastSegment.point.toString(), '{ x: 100, y: 75 }', 'We expect the last segment point to be at the position where we wanted to draw the arc to.');
+      expect(path.lastSegment.point.toString(), '{ x: 100, y: 75 }', 'We expect the last segment point to be at the position where we wanted to draw the arc to.');
     });
 
     test('path.arcTo(from, through, to); where from, through and to all share the same y position and through lies to the right of to', () {
@@ -51,7 +51,7 @@ TestFunctionName() {
       } catch (e) {
         error = e;
       }
-      equals(error != null, true, 'We expect this arcTo() command to throw an error');
+      expect(error != null, true, 'We expect this arcTo() command to throw an error');
     });
 
     test('path.arcTo(from, through, to); where from, through and to all share the same y position and through lies to the left of to', () {
@@ -65,7 +65,7 @@ TestFunctionName() {
       } catch (e) {
         error = e;
       }
-      equals(error != null, true, 'We expect this arcTo() command to throw an error');
+      expect(error != null, true, 'We expect this arcTo() command to throw an error');
     });
     
   });
