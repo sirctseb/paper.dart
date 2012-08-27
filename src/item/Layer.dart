409d4002f6ff82d72c0dd9561657aaa5e7e9c192
Christopher Best <bestchris@gmail.com>
2012-08-27 19:35:36 -0400

    tabs to spaces

--- src/item/Layer.dart
+++ src/item/Layer.dart
@@ -27,87 +27,87 @@
  * @extends Group
  */
 var Layer = this.Layer = Group.extend(/** @lends Layer# */{
-	// DOCS: improve constructor code example.
-	/**
-	 * Creates a new Layer item and places it at the end of the
-	 * {@link Project#layers} array. The newly created layer will be activated,
-	 * so all newly created items will be placed within it.
-	 *
-	 * @param {Item[]} [children] An array of items that will be added to the
-	 * newly created layer.
-	 *
-	 * @example
-	 * var layer = new Layer();
-	 */
-	initialize: function(items) {
-		this._project = paper.project;
-		// Push it onto project.layers and set index:
-		this._index = this._project.layers.push(this) - 1;
-		this.base.apply(this, arguments);
-		this.activate();
-	},
+  // DOCS: improve constructor code example.
+  /**
+   * Creates a new Layer item and places it at the end of the
+   * {@link Project#layers} array. The newly created layer will be activated,
+   * so all newly created items will be placed within it.
+   *
+   * @param {Item[]} [children] An array of items that will be added to the
+   * newly created layer.
+   *
+   * @example
+   * var layer = new Layer();
+   */
+  initialize: function(items) {
+    this._project = paper.project;
+    // Push it onto project.layers and set index:
+    this._index = this._project.layers.push(this) - 1;
+    this.base.apply(this, arguments);
+    this.activate();
+  },
 
-	/**
-	* Removes the layer from its project's layers list
-	* or its parent's children list.
-	*/
-	_remove: function(deselect, notify) {
-		if (this._parent)
-			return this.base(deselect, notify);
-		if (this._index != null) {
-			if (deselect)
-				this.setSelected(false);
-			Base.splice(this._project.layers, null, this._index, 1);
-			// Tell project we need a redraw. This is similar to _changed()
-			// mechanism.
-			this._project._needsRedraw();
-			return true;
-		}
-		return false;
-	},
+  /**
+  * Removes the layer from its project's layers list
+  * or its parent's children list.
+  */
+  _remove: function(deselect, notify) {
+    if (this._parent)
+      return this.base(deselect, notify);
+    if (this._index != null) {
+      if (deselect)
+        this.setSelected(false);
+      Base.splice(this._project.layers, null, this._index, 1);
+      // Tell project we need a redraw. This is similar to _changed()
+      // mechanism.
+      this._project._needsRedraw();
+      return true;
+    }
+    return false;
+  },
 
-	getNextSibling: function() {
-		return this._parent ? this.base()
-				: this._project.layers[this._index + 1] || null;
-	},
+  getNextSibling: function() {
+    return this._parent ? this.base()
+        : this._project.layers[this._index + 1] || null;
+  },
 
-	getPreviousSibling: function() {
-		return this._parent ? this.base()
-				: this._project.layers[this._index - 1] || null;
-	},
+  getPreviousSibling: function() {
+    return this._parent ? this.base()
+        : this._project.layers[this._index - 1] || null;
+  },
 
-	/**
-	 * Activates the layer.
-	 *
-	 * @example
-	 * var firstLayer = project.activeLayer;
-	 * var secondLayer = new Layer();
-	 * console.log(project.activeLayer == secondLayer); // true
-	 * firstLayer.activate();
-	 * console.log(project.activeLayer == firstLayer); // true
-	 */
-	activate: function() {
-		this._project.activeLayer = this;
-	}
+  /**
+   * Activates the layer.
+   *
+   * @example
+   * var firstLayer = project.activeLayer;
+   * var secondLayer = new Layer();
+   * console.log(project.activeLayer == secondLayer); // true
+   * firstLayer.activate();
+   * console.log(project.activeLayer == firstLayer); // true
+   */
+  activate: function() {
+    this._project.activeLayer = this;
+  }
 }, new function () {
-	function insert(above) {
-		return function(item) {
-			// If the item is a layer and contained within Project#layers, use
-			// our own version of move().
-			if (item instanceof Layer && !item._parent
-						&& this._remove(false, true)) {
-				Base.splice(item._project.layers, [this],
-						item._index + (above ? 1 : -1), 0);
-				this._setProject(item._project);
-				return true;
-			}
-			return this.base(item);
-		};
-	}
+  function insert(above) {
+    return function(item) {
+      // If the item is a layer and contained within Project#layers, use
+      // our own version of move().
+      if (item instanceof Layer && !item._parent
+            && this._remove(false, true)) {
+        Base.splice(item._project.layers, [this],
+            item._index + (above ? 1 : -1), 0);
+        this._setProject(item._project);
+        return true;
+      }
+      return this.base(item);
+    };
+  }
 
-	return {
-		insertAbove: insert(true),
+  return {
+    insertAbove: insert(true),
 
-		insertBelow: insert(false)
-	};
+    insertBelow: insert(false)
+  };
 });
