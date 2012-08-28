f56eb6dc6065d12fb5a021a51ba707be0956e27b
Christopher Best <bestchris@gmail.com>
2012-08-28 15:08:00 -0400

    tabs to spaces

--- src/item/PlacedSymbol.dart
+++ src/item/PlacedSymbol.dart
@@ -23,87 +23,87 @@
  * @extends PlacedItem
  */
 var PlacedSymbol = this.PlacedSymbol = PlacedItem.extend(/** @lends PlacedSymbol# */{
-	/**
-	 * Creates a new PlacedSymbol Item.
-	 *
-	 * @param {Symbol} symbol the symbol to place
-	 * @param {Point|Matrix} [pointOrMatrix] the center point of the placed
-	 * symbol or a {@link Matrix} transformation to transform the placed symbol
-	 * with.
-	 *
-	 * @example {@paperscript split=true height=240}
-	 * // Placing 100 instances of a symbol:
-	 * var path = new Path.Star(new Point(0, 0), 6, 5, 13);
-	 * path.style = {
-	 *     fillColor: 'white',
-	 *     strokeColor: 'black'
-	 * };
+  /**
+   * Creates a new PlacedSymbol Item.
+   *
+   * @param {Symbol} symbol the symbol to place
+   * @param {Point|Matrix} [pointOrMatrix] the center point of the placed
+   * symbol or a {@link Matrix} transformation to transform the placed symbol
+   * with.
+   *
+   * @example {@paperscript split=true height=240}
+   * // Placing 100 instances of a symbol:
+   * var path = new Path.Star(new Point(0, 0), 6, 5, 13);
+   * path.style = {
+   *     fillColor: 'white',
+   *     strokeColor: 'black'
+   * };
      *
-	 * // Create a symbol from the path:
-	 * var symbol = new Symbol(path);
-	 *
-	 * // Remove the path:
-	 * path.remove();
+   * // Create a symbol from the path:
+   * var symbol = new Symbol(path);
+   *
+   * // Remove the path:
+   * path.remove();
      *
-	 * // Place 100 instances of the symbol:
-	 * for (var i = 0; i < 100; i++) {
-	 *     // Place an instance of the symbol in the project:
-	 *     var instance = new PlacedSymbol(symbol);
+   * // Place 100 instances of the symbol:
+   * for (var i = 0; i < 100; i++) {
+   *     // Place an instance of the symbol in the project:
+   *     var instance = new PlacedSymbol(symbol);
      *
-	 *     // Move the instance to a random position within the view:
-	 *     instance.position = Point.random() * view.size;
+   *     // Move the instance to a random position within the view:
+   *     instance.position = Point.random() * view.size;
      *
-	 *     // Rotate the instance by a random amount between
-	 *     // 0 and 360 degrees:
-	 *     instance.rotate(Math.random() * 360);
+   *     // Rotate the instance by a random amount between
+   *     // 0 and 360 degrees:
+   *     instance.rotate(Math.random() * 360);
      *
-	 *     // Scale the instance between 0.25 and 1:
-	 *     instance.scale(0.25 + Math.random() * 0.75);
-	 * }
-	 */
-	initialize: function(symbol, pointOrMatrix) {
-		this.base(pointOrMatrix);
-		this.setSymbol(symbol instanceof Symbol ? symbol : new Symbol(symbol));
-	},
+   *     // Scale the instance between 0.25 and 1:
+   *     instance.scale(0.25 + Math.random() * 0.75);
+   * }
+   */
+  initialize: function(symbol, pointOrMatrix) {
+    this.base(pointOrMatrix);
+    this.setSymbol(symbol instanceof Symbol ? symbol : new Symbol(symbol));
+  },
 
-	/**
-	 * The symbol that the placed symbol refers to.
-	 *
-	 * @type Symbol
-	 * @bean
-	 */
-	getSymbol: function() {
-		return this._symbol;
-	},
+  /**
+   * The symbol that the placed symbol refers to.
+   *
+   * @type Symbol
+   * @bean
+   */
+  getSymbol: function() {
+    return this._symbol;
+  },
 
-	setSymbol: function(symbol) {
-		// Remove from previous symbol's instances
-		if (this._symbol)
-			delete this._symbol._instances[this._id];
-		this._symbol = symbol;
-		// Add to the new one's
-		symbol._instances[this._id] = this;
-	},
+  setSymbol: function(symbol) {
+    // Remove from previous symbol's instances
+    if (this._symbol)
+      delete this._symbol._instances[this._id];
+    this._symbol = symbol;
+    // Add to the new one's
+    symbol._instances[this._id] = this;
+  },
 
-	clone: function() {
-		return this._clone(new PlacedSymbol(this.symbol, this._matrix.clone()));
-	},
+  clone: function() {
+    return this._clone(new PlacedSymbol(this.symbol, this._matrix.clone()));
+  },
 
-	_getBounds: function(type, matrix) {
-		// Redirect the call to the symbol definition to calculate the bounds
-		// TODO: Implement bounds caching through passing on of cacheItem, so
-		// that Symbol#_changed() notification become unnecessary!
-		return this.symbol._definition._getCachedBounds(type, matrix);
-	},
+  _getBounds: function(type, matrix) {
+    // Redirect the call to the symbol definition to calculate the bounds
+    // TODO: Implement bounds caching through passing on of cacheItem, so
+    // that Symbol#_changed() notification become unnecessary!
+    return this.symbol._definition._getCachedBounds(type, matrix);
+  },
 
-	draw: function(ctx, param) {
-		Item.draw(this.symbol._definition, ctx, param);
-	},
+  draw: function(ctx, param) {
+    Item.draw(this.symbol._definition, ctx, param);
+  },
 
-	drawSelected: function(ctx, matrix) {
-		Item.drawSelectedBounds(this.symbol._definition.getBounds(), ctx,
-				matrix);
-	}
+  drawSelected: function(ctx, matrix) {
+    Item.drawSelectedBounds(this.symbol._definition.getBounds(), ctx,
+        matrix);
+  }
 
-	// TODO: PlacedSymbol#embed()
+  // TODO: PlacedSymbol#embed()
 });
