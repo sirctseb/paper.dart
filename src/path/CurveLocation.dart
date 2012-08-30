bba5c40a0005611536a7c8e931056855419152aa
Christopher Best <bestchris@gmail.com>
2012-08-29 22:48:13 -0400

    tabs to spaces

--- src/path/CurveLocation.dart
+++ src/path/CurveLocation.dart
@@ -27,183 +27,183 @@
  * {@link Path#getLocationAt(offset)}, Path#getNearestLocation(point), etc.
  */
 CurveLocation = Base.extend(/** @lends CurveLocation# */{
-	// DOCS: CurveLocation class description: add these back when the  mentioned
-	// functioned have been added: {@link Path#split(location)},
-	// {@link PathItem#getIntersections(path)}, etc.
-	/**
-	 * Creates a new CurveLocation object.
-	 *
-	 * @param {Curve} curve
-	 * @param {Number} parameter
-	 * @param {Point} point
-	 */
-	initialize: function(curve, parameter, point, distance) {
-		this._curve = curve;
-		this._parameter = parameter;
-		this._point = point;
-		this._distance = distance;
-	},
+  // DOCS: CurveLocation class description: add these back when the  mentioned
+  // functioned have been added: {@link Path#split(location)},
+  // {@link PathItem#getIntersections(path)}, etc.
+  /**
+   * Creates a new CurveLocation object.
+   *
+   * @param {Curve} curve
+   * @param {Number} parameter
+   * @param {Point} point
+   */
+  initialize: function(curve, parameter, point, distance) {
+    this._curve = curve;
+    this._parameter = parameter;
+    this._point = point;
+    this._distance = distance;
+  },
 
-	/**
-	 * The segment of the curve which is closer to the described location.
-	 *
-	 * @type Segment
-	 * @bean
-	 */
-	getSegment: function() {
-		if (!this._segment) {
-			var curve = this._curve,
-				parameter = this.getParameter();
-			if (parameter == 0) {
-				this._segment = curve._segment1;
-			} else if (parameter == 1) {
-				this._segment = curve._segment2;
-			} else if (parameter == null) {
-				return null;
-			} else {
-				// Determine the closest segment by comparing curve lengths
-				this._segment = curve.getLength(0, parameter)
-					< curve.getLength(parameter, 1)
-						? curve._segment1
-						: curve._segment2;
-			}
-		}
-		return this._segment;
-	},
+  /**
+   * The segment of the curve which is closer to the described location.
+   *
+   * @type Segment
+   * @bean
+   */
+  getSegment: function() {
+    if (!this._segment) {
+      var curve = this._curve,
+        parameter = this.getParameter();
+      if (parameter == 0) {
+        this._segment = curve._segment1;
+      } else if (parameter == 1) {
+        this._segment = curve._segment2;
+      } else if (parameter == null) {
+        return null;
+      } else {
+        // Determine the closest segment by comparing curve lengths
+        this._segment = curve.getLength(0, parameter)
+          < curve.getLength(parameter, 1)
+            ? curve._segment1
+            : curve._segment2;
+      }
+    }
+    return this._segment;
+  },
 
-	/**
-	 * The curve by which the location is defined.
-	 *
-	 * @type Curve
-	 * @bean
-	 */
-	getCurve: function() {
-		return this._curve;
-	},
+  /**
+   * The curve by which the location is defined.
+   *
+   * @type Curve
+   * @bean
+   */
+  getCurve: function() {
+    return this._curve;
+  },
 
-	/**
-	 * The path this curve belongs to, if any.
-	 *
-	 * @type Item
-	 * @bean
-	 */
-	getPath: function() {
-		return this._curve && this._curve._path;
-	},
+  /**
+   * The path this curve belongs to, if any.
+   *
+   * @type Item
+   * @bean
+   */
+  getPath: function() {
+    return this._curve && this._curve._path;
+  },
 
-	/**
-	 * The index of the curve within the {@link Path#curves} list, if the
-	 * curve is part of a {@link Path} item.
-	 *
-	 * @type Index
-	 * @bean
-	 */
-	getIndex: function() {
-		return this._curve && this._curve.getIndex();
-	},
+  /**
+   * The index of the curve within the {@link Path#curves} list, if the
+   * curve is part of a {@link Path} item.
+   *
+   * @type Index
+   * @bean
+   */
+  getIndex: function() {
+    return this._curve && this._curve.getIndex();
+  },
 
-	/**
-	 * The length of the path from its beginning up to the location described
-	 * by this object.
-	 *
-	 * @type Number
-	 * @bean
-	 */
-	getOffset: function() {
-		var path = this._curve && this._curve._path;
-		return path && path._getOffset(this);
-	},
+  /**
+   * The length of the path from its beginning up to the location described
+   * by this object.
+   *
+   * @type Number
+   * @bean
+   */
+  getOffset: function() {
+    var path = this._curve && this._curve._path;
+    return path && path._getOffset(this);
+  },
 
-	/**
-	 * The length of the curve from its beginning up to the location described
-	 * by this object.
-	 *
-	 * @type Number
-	 * @bean
-	 */
-	getCurveOffset: function() {
-		var parameter = this.getParameter();
-		return parameter != null && this._curve
-				&& this._curve.getLength(0, parameter);
-	},
+  /**
+   * The length of the curve from its beginning up to the location described
+   * by this object.
+   *
+   * @type Number
+   * @bean
+   */
+  getCurveOffset: function() {
+    var parameter = this.getParameter();
+    return parameter != null && this._curve
+        && this._curve.getLength(0, parameter);
+  },
 
-	/**
-	 * The curve parameter, as used by various bezier curve calculations. It is
-	 * value between {@code 0} (beginning of the curve) and {@code 1} (end of
-	 * the curve).
-	 *
-	 * @type Number
-	 * @bean
-	 */
-	getParameter: function() {
-		if (this._parameter == null && this._curve && this._point)
-			this._parameter = this._curve.getParameterAt(this._point);
-		return this._parameter;
-	},
+  /**
+   * The curve parameter, as used by various bezier curve calculations. It is
+   * value between {@code 0} (beginning of the curve) and {@code 1} (end of
+   * the curve).
+   *
+   * @type Number
+   * @bean
+   */
+  getParameter: function() {
+    if (this._parameter == null && this._curve && this._point)
+      this._parameter = this._curve.getParameterAt(this._point);
+    return this._parameter;
+  },
 
-	/**
-	 * The point which is defined by the {@link #curve} and
-	 * {@link #parameter}.
-	 *
-	 * @type Point
-	 * @bean
-	 */
-	getPoint: function() {
-		if (!this._point && this._curve && this._parameter != null)
-			this._point = this._curve.getPoint(this._parameter);
-		return this._point;
-	},
+  /**
+   * The point which is defined by the {@link #curve} and
+   * {@link #parameter}.
+   *
+   * @type Point
+   * @bean
+   */
+  getPoint: function() {
+    if (!this._point && this._curve && this._parameter != null)
+      this._point = this._curve.getPoint(this._parameter);
+    return this._point;
+  },
 
-	/**
-	 * The tangential vector to the {@link #curve} at the given location.
-	 *
-	 * @type Point
-	 * @bean
-	 */
-	getTangent: function() {
-		var parameter = this.getParameter();
-		return parameter != null && this._curve
-				&& this._curve.getTangent(parameter);
-	},
+  /**
+   * The tangential vector to the {@link #curve} at the given location.
+   *
+   * @type Point
+   * @bean
+   */
+  getTangent: function() {
+    var parameter = this.getParameter();
+    return parameter != null && this._curve
+        && this._curve.getTangent(parameter);
+  },
 
-	/**
-	 * The normal vector to the {@link #curve} at the given location.
-	 *
-	 * @type Point
-	 * @bean
-	 */
-	getNormal: function() {
-		var parameter = this.getParameter();
-		return parameter != null && this._curve
-				&& this._curve.getNormal(parameter);
-	},
+  /**
+   * The normal vector to the {@link #curve} at the given location.
+   *
+   * @type Point
+   * @bean
+   */
+  getNormal: function() {
+    var parameter = this.getParameter();
+    return parameter != null && this._curve
+        && this._curve.getNormal(parameter);
+  },
 
-	/**
-	 * The distance from the queried point to the returned location.
-	 *
-	 * @type Number
-	 * @bean
-	 */
-	getDistance: function() {
-		return this._distance;
-	},
+  /**
+   * The distance from the queried point to the returned location.
+   *
+   * @type Number
+   * @bean
+   */
+  getDistance: function() {
+    return this._distance;
+  },
 
-	/**
-	 * @return {String} A string representation of the curve location.
-	 */
-	toString: function() {
-		var parts = [],
-			point = this.getPoint();
-		if (point)
-			parts.push('point: ' + point);
-		var index = this.getIndex();
-		if (index != null)
-			parts.push('index: ' + index);
-		var parameter = this.getParameter();
-		if (parameter != null)
-			parts.push('parameter: ' + Base.formatNumber(parameter));
-		if (this._distance != null)
-			parts.push('distance: ' + Base.formatNumber(this._distance));
-		return '{ ' + parts.join(', ') + ' }';
-	}
+  /**
+   * @return {String} A string representation of the curve location.
+   */
+  toString: function() {
+    var parts = [],
+      point = this.getPoint();
+    if (point)
+      parts.push('point: ' + point);
+    var index = this.getIndex();
+    if (index != null)
+      parts.push('index: ' + index);
+    var parameter = this.getParameter();
+    if (parameter != null)
+      parts.push('parameter: ' + Base.formatNumber(parameter));
+    if (this._distance != null)
+      parts.push('distance: ' + Base.formatNumber(this._distance));
+    return '{ ' + parts.join(', ') + ' }';
+  }
 });
